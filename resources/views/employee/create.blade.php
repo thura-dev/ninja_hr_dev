636915876be3b1ca3f6deb31b603a01149e1e6bf
thuraaung <thuraaung7758@gmail.com>
2022-04-16 12:51:35 +0630

    change employee ui

--- resources/views/employee/create.blade.php
+++ resources/views/employee/create.blade.php
@@ -13,39 +13,19 @@
                 <div class="md-form">
                     <label for="">Name</label>
 
-                    <input type="text" name="neme" id="name" class="form-control">
+                    <input type="text" name="name" id="name" class="form-control">
                 </div>
                 <div class="md-form">
                     <label for="">Phone</label>
-                    <input type="number" name="neme" id="phone" class="form-control">
+                    <input type="number" name="phone" id="phone" class="form-control">
                 </div>
                 <div class="md-form">
                     <label for="">Email</label>
-                    <input type="email" name="neme" id="email" class="form-control">
+                    <input type="email" name="email" id="email" class="form-control">
                 </div>
                 <div class="md-form">
                     <label for="">NRC Number</label>
                     <input type="text" name="nrc_number" class="form-control">
-                </div>
-                <div class="">
-                    <label for="">Gender</label>
-                    <select name="gender" class="form-control">
-                        <option value="Male">Male</option>
-                        <option value="Female">Female</option>
-
-                    <input type="text" name="name" class="form-control">
-                </div>
-                <div class="md-form">
-                    <label for="">Phone</label>
-                    <input type="number" name="phone" id="phone" class="form-control p-1">
-                </div>
-                <div class="md-form">
-                    <label for="">Email</label>
-                    <input type="email" name="email" class="form-control">
-                </div>
-                <div class="md-form">
-                    <label for="">NRC Number</label>
-                    <input type="number" name="nrc_number" class="form-control">
                 </div>
                 <div class="form-group">
                     <label for="">Gender</label>
@@ -56,9 +36,50 @@
                 </div>
                 <div class="md-form">
                     <label for="">Birthday</label>
-
-                    <input type="text" name="nrc_number" class="form-control birthday">
+                    <input type="text" name="birthday" class="form-control birthday">
                 </div>
+                <div class="md-form">
+                    <label>Address</label>
+                    <textarea class="md-textarea form-control" rows="3" name="address"></textarea>
+                </div>
+                <div class="form-group">
+                    <label for="">Department</label>
+                    <select name="department_id" class="form-control">
+                        @foreach ($departments as $department)
+                        <option value="{{ $department->id }}">{{ $department->title }}</option>
+                        @endforeach
+                    </select>
+                </div>
+                <div class="md-form">
+                    <label for="">Date of Join</label>
+                    <input type="text" name="date_of_join" class="form-control date_of_join">
+                </div>
+                <div class="form-group">
+                    <label for="">Is Present?</label>
+                    <select name="is_present" class="form-control">
+                        <option value="1">Yes</option>
+                        <option value="0">No</option>
+                    </select>
+                </div>
+                <div class="form-group">
+                    <label for="progile_img">Profile Image</label>
+                    <input type="file" name="profile_img" id="profile_img" class="form-control">
+                    <div class="preview_img py-2">
+
+                    </div>
+                <div class="md-form">
+                    <label for="">Password</label>
+                    <input type="password" name="password" class="form-control">
+                </div>
+                <div class="d-flex justify-content-center mt-5 mb-3">
+                    <div class="col-md-6">
+                        <button type="submit" class="btn btn-theme btn-block btn-sm">Confirm</button>
+                    </div>
+                </div>
+
+
+
+
 
             </form>
         </div>
